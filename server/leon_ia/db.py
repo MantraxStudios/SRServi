5cf0622b3fef97b0e3cd99badcc9e70a869060e5
Josin <85538374+MantraxStudios@users.noreply.github.com>
2026-07-09 22:07:43 -0400

    Update db.py

--- server/leon_ia/db.py
+++ server/leon_ia/db.py
@@ -6,7 +6,7 @@ DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
     "port": int(os.getenv("DB_PORT", "3306")),
     "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", "SDVDttniogreireg@2024"),
+    "password": os.getenv("DB_PASSWORD", "@Fam#+234"),
     "database": os.getenv("DB_NAME", "srservi")
 }
 
